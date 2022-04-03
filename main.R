7a230169c385af2619c61802855346cf81a4bb91
Felipe B <20200754@esen.edu.sv>
2022-04-03 16:32:55 -0600

    Arreglo de orden de código

--- main.R
+++ main.R
@@ -1,5 +1,22 @@
 source("./distribution_functions.R")
 
 
+plot_normal_distribution(fill="pink", color="black")
 
-plot_normal_distribution(fill="red", color="blue")
+plot_gamma_distribution(fill="pink", color="black")
+
+plot_exponential_distribution(fill="pink", color="black")
+
+plot_triangular_distribution(fill="pink", color="black")
+
+plot_beta_distribution(fill="pink", color="black")
+
+plot_weibull_distribution(fill="pink", color="black")
+
+plot_uniform_distribution(fill="pink", color="black")
+
+plot_erlang_distribution(fill="pink", color="black")
+
+plot_lognormal_distribution(fill="pink", color="black")
+
+plot_normal_distribution(fill="pink", color="black")
